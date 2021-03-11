a7475e8d26605f163982324a5204dc8b3b9730de
Mayra Azevedo <mayradazevedo@gmail.com>
2021-03-11 19:53:52 -0300

    add django_heroku

--- registro_epidemiologico/settings.py
+++ registro_epidemiologico/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,13 +135,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_URL = '/static/'
-
 # try to load local_settings.py if it exists
 try:
   from .local_settings import *
 except Exception as e:
   pass
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
