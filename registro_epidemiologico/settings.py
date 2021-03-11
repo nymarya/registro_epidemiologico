9035b9877328d0210b29e35fd43e43025b7a14bc
Mayra Azevedo <mayradazevedo@gmail.com>
2021-03-11 19:56:21 -0300

    add django_heroku

--- registro_epidemiologico/settings.py
+++ registro_epidemiologico/settings.py
@@ -60,14 +60,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'registro_epidemiologico.urls'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -133,6 +125,14 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # try to load local_settings.py if it exists
